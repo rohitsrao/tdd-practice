de974a5cbe46280b5c2f47104c496725b23e7426
Rohit <rohitsrao@gmail.com>
2021-08-24 15:53:45 +0200

    Returns zero for empty string

--- 02_string_calc/test_string_calc.py
+++ 02_string_calc/test_string_calc.py
@@ -1,9 +1,12 @@
 import unittest
 
+def add(inp_str):
+    return 0
+
 class StringCalculatorTest(unittest.TestCase):
 
-    def test(self):
-        pass
+    def test_given_empty_string_returns_zero(self):
+        self.assertEqual(add(''), 0)
 
 if __name__ == '__main__':
     unittest.main()
