8edd64e78def33311e718542707339ae2ccf878d
Rohit <rohitsrao@gmail.com>
2021-08-24 08:29:04 +0200

    tests if input is not a string

--- 02_string_calc/test_string_calc.py
+++ 02_string_calc/test_string_calc.py
@@ -3,8 +3,11 @@ import unittest
 class StringCalculator:
 
     def add(self, inp_str):
-        if not inp_str:
-            return 0
+        if isinstance(inp_str, str):
+            if not inp_str:
+                return 0
+        else:
+            return -1
 
 class StringCalculatorTest(unittest.TestCase):
 
@@ -14,6 +17,10 @@ class StringCalculatorTest(unittest.TestCase):
     def test_given_empty_string_returns_zero(self):
         result = self.str_calc.add('')
         self.assertEqual(result, 0)
+
+    def test_given_non_string_input_return_minus_one(self):
+        result = self.str_calc.add(5)
+        self.assertEqual(result, -1)
 
 if __name__ == '__main__':
     unittest.main()
