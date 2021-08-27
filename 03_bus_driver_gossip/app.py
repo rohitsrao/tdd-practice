aa8c8c9900dfac99d8c771c8db05598b2adbeb0a
Rohit <rohitsrao@gmail.com>
2021-08-27 07:38:01 +0200

    Refactored and created App.update_all_driver_stops
    as separate method from increment_timestep().
    Eliminated initialize_drivers()

--- 03_bus_driver_gossip/app.py
+++ 03_bus_driver_gossip/app.py
@@ -4,13 +4,16 @@ class App:
         self.list_of_drivers = list_of_drivers
         self.timestep = 0
 
-    def initialize_drivers(self):
-        for driver in self.list_of_drivers:
-            driver.start()
+    def increment_timestep(self):
+        self.timestep += 1
 
     def generate_stop_list(self):
         route_concatenation = []
         for driver in self.list_of_drivers:
             route_concatenation += driver.route
         self.stops = set(route_concatenation)
+
+    def update_all_driver_stops(self):
+        for driver in self.list_of_drivers:
+            driver.update_stop()
 
--- 03_bus_driver_gossip/test_app.py
+++ 03_bus_driver_gossip/test_app.py
@@ -19,22 +19,24 @@ class TestApp(unittest.TestCase):
         self.assertTrue(hasattr(self.app, 'list_of_drivers'))
         self.assertIsInstance(self.app.list_of_drivers, list)
     
-    def test_initalize_drivers_method(self):
-        self.assertTrue(hasattr(self.app, 'initialize_drivers'))
-    
     def test_timestep_attribute(self):
         self.assertTrue(hasattr(self.app, 'timestep'))
         self.assertEqual(self.app.timestep, 0)
-    
-    def test_after_calling_initialize_drivers_current_pos_of_all_buses_updated(self):
-        self.app.initialize_drivers()
-        for bus_driver in self.list_of_drivers:
-            self.assertEqual(bus_driver.current_pos, bus_driver.route[0])
     
     def test_calling_generate_stop_set_creates_non_empty_set_attribute_stops(self):
         self.app.generate_stop_list()
         self.assertIsInstance(self.app.stops, set)
         self.assertNotEqual(len(self.app.stops), 0)
+
+    def test_calling_increment_timestep_updates_timestep(self):
+        self.app.increment_timestep()
+        self.app.increment_timestep()
+        self.assertEqual(self.app.timestep, 2)
+
+    def test_calling_update_all_driver_stops(self):
+        self.app.update_all_driver_stops()
+        for driver in self.list_of_drivers:
+            self.assertEqual(driver.current_stop, driver.route[1])
 
 
 if __name__ == '__main__':
